7091aed5b991ff97ce499072693f4edff0514d6b
shirakasu <antith.private@gmail.com>
2025-12-30 17:39:53 +0900

    neovim plugin update

--- .zshrc
+++ .zshrc
@@ -138,6 +138,7 @@ export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/gcc-arm-none-eabi-6-2017-q1-update/bin:$PATH"
 
 # Neovim PATH
+export PATH="$HOME/nvim-macos-arm64/bin:$PATH"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 
 # pyenv PATH
---
+++ nvim/lua/plugins/tabset.lua
@@ -0,0 +1,73 @@
+return {
+    "FotiadisM/tabset.nvim",
+    opts = {
+        defaults = {
+            tabwidth = 4,
+            expandtab = true
+        },
+
+        languages = {
+            python = {
+                tabwidth = 4,
+                expandtab = true
+            },
+
+            rust = {
+                tabwidth = 4,
+                expandtab = true
+            },
+
+            java = {
+                tabwidth = 4,
+                expandtab = true
+            },
+
+            html = {
+                tabwidth = 2,
+                expandtab = true
+            },
+
+            css = {
+                tabwidth = 2,
+                expandtab = true
+            },
+
+            javascript = {
+                tabwidth = 2,
+                expandtab = true
+            },
+
+            typescript = {
+                tabwidth = 2,
+                expandtab = true
+            },
+
+            javascriptreact = {
+                tabwidth = 2,
+                expandtab = true
+            },
+
+            typescriptreact = {
+                tabwidth = 2,
+                expandtab = true
+            },
+
+            ruby = {
+                tabwidth = 2,
+                expandtab = true
+            },
+
+            c = {
+                tabwidth = 2,
+                expandtab = true
+            },
+
+            cpp = {
+                tabwidth = 2,
+                expandtab = true
+            },
+
+        }
+
+    }
+}
